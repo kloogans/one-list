a4704c743abea1cab7ee964fb7e3b47058417390
James O'Brien <jtwebco@gmail.com>
2017-03-28 14:14:52 -0400

    Project build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+// One List
+const input = document.querySelector('input')
+const list = document.querySelector('ul.list')
+// const enter = document.querySelector('input')
+const button = document.querySelector('button')
+const form = document.querySelector('form')
 
-document.addEventListener('DOMContentLoaded', main)
+button.addEventListener('click', (event) => {
+  event.preventDefault()
+  const listItem = input.value
+  const li = document.createElement('li')
+  li.textContent = listItem
+  list.appendChild(li)
+  li.addEventListener('click', () => {
+    li.style.textDecoration = 'line-through'
+  })
+  li.addEventListener('dblclick', () => {
+    list.removeChild(li)
+  })
+  form.reset()
+})
 
+// IE9
 if (module.hot) {
   module.hot.dispose(() => window.location.reload())
   module.hot.accept(err => console.error(err))
