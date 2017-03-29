df4b05ad07386437cc136b4ffa9f9d4694ae0124
James O'Brien <jtwebco@gmail.com>
2017-03-28 23:31:35 -0400

    Cleans up code and changes background

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
-// One List
+// Select elements
 const input = document.querySelector('input')
 const list = document.querySelector('ul.list')
-// const enter = document.querySelector('input')
 const button = document.querySelector('button')
 const form = document.querySelector('form')
 
+// Listen for button "click"
 button.addEventListener('click', (event) => {
+  // Prevent reload on submit
   event.preventDefault()
+  // Get value from text input
   const listItem = input.value
   const li = document.createElement('li')
   li.setAttribute('class', 'animated fadeIn')
+  // Put Input value into <li> tags
   li.textContent = listItem
+  // Inject new <li> into list
   list.appendChild(li)
   let didDblClick = false
+  // Delay single click action
   li.addEventListener('click', () => {
     setTimeout(() => {
       if (!didDblClick) {
         li.style.textDecoration = 'line-through'
-        li.style.backgroundColor = '#333'
+        li.style.backgroundColor = 'rgba(51, 51, 51, 0.80)'
       }
     }, 300)
   })
+  // Remove list item on double click
   li.addEventListener('dblclick', () => {
     didDblClick = true
     list.removeChild(li)
@@ -31,7 +37,6 @@ button.addEventListener('click', (event) => {
   form.reset()
 })
 
-// IE9
 if (module.hot) {
   module.hot.dispose(() => window.location.reload())
   module.hot.accept(err => console.error(err))
